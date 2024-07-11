b6dddb0767e9322985a30ef2024f689d83d6407e
rsharpe23 <roman.sharpe23@gmail.com>
2024-07-11 10:12:06 +0300

    11.07.2024, 10:12

--- src/renderer.js
+++ src/renderer.js
@@ -1,23 +1,14 @@
 export default class {
   _isReady = false;
 
-  // render(appProps) {
-  //   if (this._isReady) {
-  //     this._render(appProps);
-  //     return;
-  //   }
-
-  //   this._beforeRender?.(appProps);
-  //   this._isReady = true;
-  // }
-
   render(appProps) {
-    if (!this._isReady) {
-      this._beforeRender?.(appProps);
-      this._isReady = true;
+    if (this._isReady) {
+      this._render(appProps);
+      return;
     }
 
-    this._render(appProps);
+    this._beforeRender?.(appProps);
+    this._isReady = true;
   }
 
   _render(appProps) {

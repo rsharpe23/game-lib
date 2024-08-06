bc8ca07d18cac7455f913bee683a89ac28bcda33
rsharpe23 <roman.sharpe23@gmail.com>
2024-08-06 15:32:11 +0300

    06.08.2024, 15:32

--- shaders/@default/vert.glsl
+++ shaders/@default/vert.glsl
@@ -1,8 +1,8 @@
-attribute vec3 a_Position;
-attribute vec3 a_Normal;
+attribute vec4 a_Position;
+attribute vec4 a_Normal;
 attribute vec2 a_Texcoord;
 
-uniform mat4 u_PMatrix; 
+uniform mat4 u_PMatrix;
 uniform mat4 u_MVMatrix;
 uniform mat4 u_NMatrix;
 
@@ -14,32 +14,19 @@ uniform vec3 u_LightingPos;
 uniform vec3 u_MaterialAmbientColor;
 uniform vec3 u_MaterialSpecularColor;
 
-varying vec3 v_Color;
+varying vec4 v_Color;
 varying vec2 v_Texcoord;
 
-void main(void) {
-  float specular = 0.0;
+#pragma glslify: calcLighting = require(./lighting.glsl)
 
-  vec4 vertexPos4 =  u_MVMatrix * vec4(a_Position, 1.0);
-  vec3 vertexPos = vertexPos4.xyz;
-  vec3 eye = normalize(-vertexPos);
-
-  vec3 transformedNormal = vec3(u_NMatrix * vec4(a_Normal, 1.0));
-  vec3 normal = normalize(transformedNormal);
-
-  vec3 lightDir = normalize(u_LightingPos);
-  float lambertTerm = max(dot(normal, -lightDir), 0.0);
-
-  if (lambertTerm > 0.0) {
-    vec3 halfDir = normalize(-lightDir + eye);
-    float specAngle = max(dot(halfDir, normal), 0.0);
-    specular = pow(specAngle, 16.0);
-  }
-
-  v_Color = u_AmbientColor * u_MaterialAmbientColor 
-    + u_DiffuseColor * lambertTerm 
-    + u_SpecularColor * u_MaterialSpecularColor * specular;
-
+void main() {
   v_Texcoord = a_Texcoord;
-  gl_Position = u_PMatrix * vertexPos4;
+
+  vec4 tPos = u_MVMatrix * a_Position;
+
+  v_Color = calcLighting(tPos, u_NMatrix * a_Normal, u_LightingPos, 
+    u_AmbientColor * u_MaterialAmbientColor, u_DiffuseColor, 
+    u_SpecularColor * u_MaterialAmbientColor);
+
+  gl_Position = u_PMatrix * tPos;
 }
--- shaders/lighting.glsl
+++ shaders/lighting.glsl
@@ -1,23 +1,3 @@
-#define GLSLIFY 1
-attribute vec4 a_Position;
-attribute vec4 a_Normal;
-attribute vec2 a_Texcoord;
-
-uniform mat4 u_PMatrix;
-uniform mat4 u_MVMatrix;
-uniform mat4 u_NMatrix;
-
-uniform vec3 u_AmbientColor;
-uniform vec3 u_DiffuseColor;
-uniform vec3 u_SpecularColor;
-uniform vec3 u_LightingPos;
-
-uniform vec3 u_MaterialAmbientColor;
-uniform vec3 u_MaterialSpecularColor;
-
-varying vec4 v_Color;
-varying vec2 v_Texcoord;
-
 // Модель отражения Блинна-Фонга (с затенением Гуро, 
 // если применять в вершинном шейдере)
 
@@ -52,14 +32,4 @@ vec4 calcLighting(vec4 tPos, vec4 tNormal, vec3 lightingPos,
   return vec4(ambientColor + diffuseColor + specularColor, 1.0);
 }
 
-void main() {
-  v_Texcoord = a_Texcoord;
-
-  vec4 tPos = u_MVMatrix * a_Position;
-
-  v_Color = calcLighting(tPos, u_NMatrix * a_Normal, u_LightingPos, 
-    u_AmbientColor * u_MaterialAmbientColor, u_DiffuseColor, 
-    u_SpecularColor * u_MaterialAmbientColor);
-
-  gl_Position = u_PMatrix * tPos;
-}
+#pragma glslify: export(calcLighting)
